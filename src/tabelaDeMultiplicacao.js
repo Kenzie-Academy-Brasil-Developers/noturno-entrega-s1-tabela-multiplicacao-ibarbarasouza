9b976f43d6f87cee6196d34d4d1b221295288efc
Bárbara Souza <ibarbarasouza@icloud.com>
2021-10-20 23:29:30 -0300

    Tabela de multiplicacao com bonus

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,10 +1,11 @@
 
-function tabelaMult(n) { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // todo: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+function tabelaMult(n) {
+    
     let arrayTabela = [];
 
     for (let contador = 0; contador <= n; contador++) {
         arrayTabela [contador] = [contador * n];
+        
         for (let subContador = 0; subContador<= n; subContador++){
         arrayTabela [contador] [subContador] = [subContador * contador];
         }
