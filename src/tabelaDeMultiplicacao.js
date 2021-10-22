63a1017a76023a99b7eee265485ac43f00f79d26
Bárbara Souza <ibarbarasouza@icloud.com>
2021-10-22 17:04:34 -0300

    alteração do return

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,16 +1,17 @@
 
-function tabelaMult(n) {
+
+        function tabelaMult(n) {
     
     let arrayTabela = [];
 
     for (let contador = 0; contador <= n; contador++) {
         arrayTabela [contador] = [contador * n];
-        
+
         for (let subContador = 0; subContador<= n; subContador++){
         arrayTabela [contador] [subContador] = [subContador * contador];
         }
     }
-    return arrayTabela;
+    console.table(arrayTabela);
 }
 
-console.table(tabelaMult(9))
+tabelaMult(9)
